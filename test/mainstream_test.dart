a9310fc5763e82cd9a02881a815887be54f08400
Martin Rybak <martin.rybak@gmail.com>
2020-05-03 11:57:05 -0400

    Added tests

--- test/mainstream_test.dart
+++ test/mainstream_test.dart
@@ -1,13 +1,121 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:mainstream/mainstream.dart';
 
+// ignore_for_file: missing_required_param
+
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  group('Constructor', () {
+    testWidgets('throws assertion error if stream is null', (tester) async {
+      expect(() => MainStream(), throwsAssertionError);
+    });
+  });
+
+  group('Builders', () {
+    testWidgets('initially shows busyBuilder', (tester) async {
+      final text = 'busy';
+      final stream = Stream.value(0);
+      final widget = MaterialApp(
+        home: MainStream(
+          stream: stream,
+          busyBuilder: (_) => Text(text),
+        ),
+      );
+      await tester.pumpWidget(widget);
+      expect(find.text(text), findsOneWidget);
+    });
+
+    testWidgets('initially shows default busy widget if busyBuilder is null', (tester) async {
+      final stream = Stream.value(0);
+      final widget = MaterialApp(
+        home: MainStream(stream: stream),
+      );
+      await tester.pumpWidget(widget);
+      expect(find.byWidgetPredicate((w) => w is CircularProgressIndicator), findsOneWidget);
+    });
+
+    testWidgets('initially shows dataBuilder if initialData is not null', (tester) async {
+      final initialData = 0;
+      final nextData = 1;
+      final stream = Stream.value(nextData);
+      final widget = MaterialApp(
+        home: MainStream(
+          stream: stream,
+          initialData: initialData,
+          dataBuilder: (_, data) => Text(data.toString()),
+        ),
+      );
+      await tester.pumpWidget(widget);
+      expect(find.text(initialData.toString()), findsOneWidget);
+    });
+
+    testWidgets('shows dataBuilder after stream emits data', (tester) async {
+      final initialData = 0;
+      final nextData = 1;
+      final stream = Stream.value(nextData);
+      final widget = MaterialApp(
+        home: MainStream(
+          stream: stream,
+          initialData: initialData,
+          dataBuilder: (_, data) => Text(data.toString()),
+        ),
+      );
+      await tester.pumpWidget(widget);
+      await tester.pump();
+      expect(find.text(nextData.toString()), findsOneWidget);
+    });
+
+    testWidgets('shows errorBuilder after stream emits error', (tester) async {
+      final error = 'error';
+      final stream = Stream.error(error);
+      final widget = MaterialApp(
+        home: MainStream(
+          stream: stream,
+          errorBuilder: (_, error) => Text(error),
+        ),
+      );
+      await tester.pumpWidget(widget);
+      await tester.pump();
+      expect(find.text(error), findsOneWidget);
+    });
+  });
+
+  group('Callbacks', () {
+    testWidgets('onData called after stream emits data', (tester) async {
+      final data = 1;
+      final stream = Stream.value(data);
+      final widget = MaterialApp(
+        home: MainStream(
+          stream: stream,
+          onData: expectAsync1((d) => expect(d, data)),
+        ),
+      );
+      await tester.pumpWidget(widget);
+      await tester.pump();
+    });
+
+    testWidgets('onError called after stream emits error', (tester) async {
+      final error = 'error';
+      final stream = Stream.error(error);
+      final widget = MaterialApp(
+        home: MainStream(
+          stream: stream,
+          onError: expectAsync1((e) => expect(e, error)),
+        ),
+      );
+      await tester.pumpWidget(widget);
+      await tester.pump();
+    });
+
+    testWidgets('onDone called after stream completes', (tester) async {
+      final stream = Stream.value(0);
+      final widget = MaterialApp(
+        home: MainStream(
+          stream: stream,
+          onDone: expectAsync0(() {}),
+        ),
+      );
+      await tester.pumpWidget(widget);
+    });
   });
 }
