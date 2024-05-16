ad5eca67a3de771c09513782cbc022eb34fe9ebd
Martin Rybak <martin.rybak@gmail.com>
2024-05-15 17:01:45 -0700

    Linter code updates

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,32 +3,36 @@ import 'package:flutter/material.dart';
 import 'package:mainstream/mainstream.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Home(),
     );
   }
 }
 
-final myStream = Stream<int>.periodic(Duration(seconds: 1), (x) => (x == 3) ? throw Exception('oops') : x).take(5);
+final myStream = Stream<int>.periodic(const Duration(seconds: 1), (x) => (x == 3) ? throw Exception('oops') : x).take(5);
 
 class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: MainStream<int>(
           stream: myStream,
-          onData: (data) => print(data),
+          onData: (data) => debugPrint(data.toString()),
           onError: (error) => _showAlert(context, error.toString()),
-          onDone: () => print('Done!'),
-          busyBuilder: (_) => CircularProgressIndicator(),
+          onDone: () => debugPrint('Done!'),
+          busyBuilder: (_) => const CircularProgressIndicator(),
           dataBuilder: (_, data) => Text(data.toString()),
           errorBuilder: (_, error) => Text(error.toString()),
         ),
@@ -44,7 +48,7 @@ class Home extends StatelessWidget {
           content: Text(text),
           actions: <Widget>[
             TextButton(
-              child: Text('OK'),
+              child: const Text('OK'),
               onPressed: () => Navigator.of(context).pop(),
             )
           ],
